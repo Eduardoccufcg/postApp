e9844f1567b98e70215172d72f92750a597564e6
Eduardo Pereira <eduardo.pereira.santos@ccc.ufcg.edu.br>
2019-08-01 16:33:55 -0300

    Deleção

--- index.js
+++ index.js
@@ -1,41 +1,88 @@
-
 const express = require("express");
 const app = express();
-const handlebars  = require("express-handlebars");
-const bodyParser = require('body-parser');
-const Post = require('./models/post')
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const Post = require("./models/post");
 
 // Config
-    //Template Engine
-        app.engine('handlebars',handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars');
-    // Body Parser
-        app.use(bodyParser.urlencoded({extended: false}))
-        app.use(bodyParser.json());
-        
-        
+//Template Engine
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // ROTAS
-app.get('/cad',function(req,res){
-    res.render(__dirname +'/views/layouts/formulario')
 
+app.get("/home", function(req, res) {
+  Post.findAll({ order: [["id", "DESC"]] }).then(function(posts) {
+    res.render(__dirname + "/views/layouts/home", { posts: posts });
+  });
 });
 
-app.post('/add',function(req,res){
+app.get("/form", function(req, res) {
+  res.render(__dirname + "/views/layouts/formulario");
+});
 
-    Post.create({
-        titulo: req.body.titulo,
-        conteudo: req.body.conteudo
-
-    }).then(function(){
-        res.status(200).send("Post criado com sucesso!")
-    }).catch(function(erro){
-        res.status(500).send("Houve um erro " + erro);
+/*
+    Cria um novo post
+*/
+app.post("/", function(req, res) {
+  Post.create({
+    titulo: req.body.titulo,
+    conteudo: req.body.conteudo
+  })
+    .then(function() {
+      res.redirect("/home");
+      //res.status(200).send("Post criado com sucesso!")
+    })
+    .catch(function(erro) {
+      res.status(500).send("Houve um erro " + erro);
     });
+});
 
+/*
+    Deleta um post pelo id
+*/
+app.delete("/:id", function(req, res) {
+  Post.findByPk(req.params.id).then(post => {
+    if (post != null) {
+      Post.destroy({ where: { id: req.params.id } })
+        .then(function() {
+          res.send("Postagem deletada com sucesso!");
+        })
+        .catch(function(erro) {
+          res.send("erro");
+        });
+    } else {
+      res.send("Postagem nao existe!");
+    }
+  });
+});
+
+app.listen(8080, function() {
+  console.log("Servidor Rodando na porta 8080");
+});
+/*
+    Atualiza um post
+*/
+app.put("/:id", function(req, res) {
+  Post.findByPk(req.params.id).then(post => {
+    if (post != null) {
+      Post.update(
+        { conteudo: req.body.conteudo },
+        { where: { id: req.params.id } }
+      )
+        .then(function() {
+          res.send("Postagem atualizada com sucesso!");
+        })
+        .catch(function(erro) {
+          res.send("erro");
+        });
+    } else {
+      res.send("Postagem nao existe!");
+    }
+  });
 });
 
 
-
-app.listen(8080,function(){
-    console.log('Servidor Rodando na porta 8080');
-});
